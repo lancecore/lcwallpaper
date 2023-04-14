445311ecd705d249f9db01b3f05e116b254e0ae3
Lance Boer <lance@Lances-MacBook-Pro-2.local>
2023-04-13 22:45:12 -0700

    Name and favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,8 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" type="image/png" href="/favicon.png" media="(prefers-color-scheme: light)" />
+          <link rel="icon" type="image/png" href="/favicon-dark.png" media="(prefers-color-scheme: dark)" />
           <meta name="description" content="Weird wallpapers for your phone" />
           <meta
             property="og:description"
