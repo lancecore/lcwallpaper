93a3c56cd432824e8b61a79916bc25d11e472c69
Lance Boer <lance@Lances-MacBook-Pro-2.local>
2024-04-21 20:22:24 -0700

    Change sort_by

--- pages/index.tsx
+++ pages/index.tsx
@@ -167,7 +167,7 @@ export default Home;
 export async function getStaticProps() {
   const results = await cloudinary.v2.search
     .expression(`folder:${process.env.CLOUDINARY_FOLDER}/*`)
-    .sort_by("public_id", "desc")
+    .sort_by("created_at", "desc")
     .max_results(400)
     .execute();
   let reducedResults: ImageProps[] = [];
